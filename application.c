6d22de82a9fda1759a6c73647305730b543f773b
gbeade <67883264+gbeade@users.noreply.github.com>
2021-09-05 17:13:51 -0300

    App fork, pipes
    Added correct forking and piping of application process

--- application.c
+++ application.c
@@ -2,26 +2,48 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+ 
+#define MS 0 
+#define SM 1 
+#define RD 0 
+#define WR 1
 
 // TODO: create share memory. Application prints the name for said buffer
 
 #define QSLAVES 5 
 
 int main(int argc, char *argv[]) {
-
-    pid_t spids[QSLAVES];     
-    char * args[] = {".", NULL}; 
     
-    for ( int i=0; i<QSLAVES; i++ ) {
+    // Slave, flow direction, read|write
+    int pipes[QSLAVES][2][2]; // pipes[slave_id][ MS | SM ][ RD | WR ]
+    
+    for (int i=0; i<QSLAVES; i++) 
+        for (int j=0; j<2; j++)
+            if ( pipe(pipes[i][j]) < 0 ) {
+                perror("Unable to create pipe\n"); 
+             return 1; 
+         }  
+    
+    
+    pid_t spids[QSLAVES];     
+    char * args[] = {"MINISAT", NULL}; 
+    
+    for (int i=0; i<QSLAVES; i++) {
         spids[i] = fork(); 
         if ( spids[i] < 0 ) { 
-            perror("Error in fork\n"); 
+            perror("Unable to fork\n"); 
             return 1; 
-        } else if ( !spids[i] ) { // TODO: Este if se corre un poco redundante. Puede evitarse? 
-            execvp("./dummy", args); 
-        }
+        } else if ( !spids[i] ) {
+            close(pipes[i][MS][WR]); 
+            close(pipes[i][SM][RD]); 
+            dup2(pipes[i][MS][RD], STDIN_FILENO);
+            dup2(pipes[i][SM][WR], STDOUT_FILENO);
+            execvp("./slave", args); 
+        } 
+        close(pipes[i][MS][RD]); 
+        close(pipes[i][SM][WR]); 
     }
-    
-
 }
 

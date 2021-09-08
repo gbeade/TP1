4df936f25b55d2b8b5a2f48b7127567d856d5349
gbeade <67883264+gbeade@users.noreply.github.com>
2021-09-08 14:05:54 -0300

    Restored slave.c to Salta's version

--- slave.c
+++ slave.c
@@ -2,85 +2,64 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
-// #include <sys/wait.h>
+#include <sys/wait.h>
+#include <string.h>
 #include <stdlib.h>
 
 
-// Compile with -DSOLVER = '"path"'
+// Compile with -DSOLVER = '"solver_path"'
 #ifndef SOLVER
-    #define SOLVER "./dummy"
+    #define SOLVER "/usr/bin/minisat"
 #endif
 
-// Compile with -DFETCHER = '"path"'
-#ifndef FETCHER
-    #define FETCHER 9 
+// Compile with -DPARSER ='"cmd_parser"'
+#ifndef PARSER
+  #define PARSER "grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\""
 #endif 
 
 #define RD 0 
 #define WR 1
 #define BUFFSIZE 256 
 
-static inline int callSolver( int * slverpipe, char * args[]); // INLINE? Or normal function? 
-static int getlinefd(int fd, char * buffer, unsigned int buffsize); // getline for file descriptors
-
-// TODOS in priority
-// *****************************************************
-// TODO: Make parent wait for child to finish before getting the line
-// TODO: Add Fetcher (egrep from Ariel). popen + grep -o -e "Number of.*[0-9]\+" -e "CPU time.*" -e !! KEY ! 
-// TODO: add closes 
 // TODO: error library - index errors 
-
-
 int main(int argc, char *argv[]) {
 
     char buffer[BUFFSIZE]; 
-    char * args[2] = {buffer, NULL}; 
-    int slvrpipe[2];
 
-
-    if ( pipe(slvrpipe) < 0 ) {
-        perror("Unable to create pipe at slave.\n");
-        return 1;  
-    } 
-     
-    while ( 1 ) { //Decidir como cortar el loop
+    while (1) {
+        // El problema con usar getline es que arma un string en el heap, que despues hay que liberar. No esta bueno, es ineficiente. 
+        // Por otro lado, getline devuelve el # del buffer final; fgets no devuelve la # de caracteres repetidos
+        // Por otro lado, fgets puede trabajar con un buffer en el stack
         fgets(buffer, BUFFSIZE, stdin);
-        if ( callSolver( slvrpipe, args) ) {
-            perror("Unable to initialize solver.\n");
-            return 1; 
+        int n = strlen(buffer); //INEFICIENTE! Hay que encontrar una manera de getline sin \n pero en el stack
+        buffer[n-1] = '\0';  
+
+        FILE * parser = popen(PARSER,"w");
+        if( parser == NULL) {
+		    perror("Unable to popen the grep.\n");
+		    return -1;
+	    }
+        
+        int fd = fileno(parser);
+        if(fd < 0 ) {
+		    perror("Unable to fileno\n");
+		    return -1;
+	    }
+
+	    pid_t pid = fork();
+        if ( pid < 0 ){
+		    perror("Unable to fork\n");
+		    return -1;
+	    } else if(pid==0){
+            dup2(fd, STDOUT_FILENO); //check if fails 
+            execl(SOLVER, SOLVER, buffer, NULL);
+            perror("Unable to exec solver\n");
+            return -1; 
+	    } else{
+	        int status; 
+            waitpid(pid,&status,0);
+	        pclose(parser);
         }
-        // LO UNICO QUE FALTA ES EL WAIT 
-        getlinefd(slvrpipe[RD], buffer, BUFFSIZE); 
-        printf("%s", buffer); 
     }
+    return 0; 
 } 
-
-
-static inline int callSolver( int * slvrpipe, char * args[] ) {  
-    pid_t pid = fork(); 
-    if ( pid < 0 ) { 
-        perror("Unable to fork.\n"); 
-        return 1; 
-    } else if ( 0 == pid ) { 
-        dup2(slvrpipe[WR], STDOUT_FILENO);
-        execvp(SOLVER, args);
-        return 1; 
-    }
-    return 0; 
-}
-
-
-static int getlinefd(int fd, char * buffer, unsigned int buffsize) {
-    int i = 0;
-    while ( i<buffsize && read(fd, buffer+i, 1) > 0 &&  ( !i || buffer[i-1] != '\n') ) i ++;  
-    buffer[i] = '\0'; 
-    return 0; 
-}
-
-
-
-// Comentario interesante: file descirptor (int) vs file pointer (FILE * )
-// You can create a FILE stream out of a filedescriptor with fdopen.
- 
-// The pipe function returns is pair of int file descriptors, 
-// not FILE ones. That means that you can use read, write, or close on them but neither fgetc, nor feof.

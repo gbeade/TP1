193d1a9144da9da93328f7c4c8e0029891e8cab9
gbeade <67883264+gbeade@users.noreply.github.com>
2021-09-10 16:05:14 -0300

    Tabs for correct alignment

--- application.c
+++ application.c
@@ -15,25 +15,130 @@
 #define WR 1
 
 #define MAXPATH 256
+#QSLAVES 2
+
+void closeOther(int me , int fd[QSLAVES][2][]);
+int  convert( char * src , char * dest);
+void initializeSet(fd_set * set, int  fd[QSLAVES][2][2]);  // inline? 
 
 // TODO: create share memory. Application prints the name for said buffer
+// TODO: al principio, enviar dos o mas tareas inicialmente a cada slave
+// TODO: 
+int main(int argc, char *argv[]) {
+  
+	   
+	// Slave, flow direction, read|write
+	int pipes[QSLAVES][2][2]; // pipes[slave_id][ MS | SM ][ RD | WR ]
+	
+	for (int i=0; i<QSLAVES; i++) 
+		for (int j=0; j<2; j++)
+			if ( pipe(pipes[i][j]) < 0 ) {
+				perror("Unable to create pipe at master\n"); 
+				return 1; 
+			}  
+   
+	
+	pid_t spids[QSLAVES];     
+	char * args[] = {"./slave",NULL}; 
+	
 
-#define QSLAVES 2 
+	for (int i=0; i<QSLAVES; i++) {
+		spids[i] = fork(); 
+		if ( spids[i] == -1 ) { 
+			perror("Unable to fork\n"); 
+			return 1; 
+		} else if ( !spids[i] ) {
+			closeOther(i,pipes);
+			close(pipes[i][MS][WR]); 
+			close(pipes[i][SM][RD]); 
+			dup2(pipes[i][MS][RD], STDIN_FILENO);
+			dup2(pipes[i][SM][WR], STDOUT_FILENO);
+			execvp("./slave", args); 
+		} 
+		close(pipes[i][MS][RD]); 
+		close(pipes[i][SM][WR]); 
+	}
+
+	sleep(15);
 
 
-void closeOther(int me , int fd[QSLAVES][2][2] ) {
+	// First shipment of files to slaves 
+	int active = QSLAVES;
+	int currentPathIndex = 1;
+	char pass[50];
+	for( int i = 0 ; i < QSLAVES ; i++){
+		if( currentPathIndex < argc ){
+			int count = convert(argv[currentPathIndex],pass);
+			if( write ( pipes[i][MS][WR] , pass , count ) != count)
+				printf("Write goes wrong\n");
+			currentPathIndex++;
+		} else {
+			close(pipes[i][MS][WR]);
+			pipes[i][MS][WR]=-1;
+			active--;
+		}
+	}
+
+
+	// Recurrent shipment of files to slaves 
+	fd_set readings;
+	initializeSet(&readings, pipes);
+
+	int resfd = open("resultado",O_RDWR | O_CREAT,S_IRWXU);
+
+	int updated;
+	while(active){
+		updated = select( FD_SETSIZE ,&readings ,NULL ,NULL,NULL); 
+	   	for (int i = 0 ; i < QSLAVES && updated ; i++) {
+			if( FD_ISSET(pipes[i][SM][RD],&readings)) {
+				updated--;		
+				if( currentPathIndex < argc ){
+					int count = convert(argv[currentPathIndex],pass);
+					write(pipes[i][MS][WR],pass,count);					
+					currentPathIndex++;
+				} else { 
+					close(pipes[i][MS][WR]);
+					pipes[i][MS][WR]=-1;
+					active--;
+				}
+			}
+		}
+
+		for(int i = 0 ; i < QSLAVES ; i++){
+			if( FD_ISSET(pipes[i][SM][RD],&readings) ){
+				char tr[300];
+				int readC = read(pipes[i][SM][RD],tr,300);
+				dprintf(resfd,"Proccess number:\t%d\n",spids[i]);
+				write(resfd,tr,readC);
+			}	
+		} 
+		initializeSet(&readings,pipes);
+	}
+
+
+	for ( int i = 0 ; i < QSLAVES ; i++){
+		waitpid(spids[i],NULL,0);
+	}
+}
+
+void initializeSet(fd_set *  set, int  fd[QSLAVES][2][]){
+	FD_ZERO(set);
+	for(int i = 0 ; i < QSLAVES ; i++) {
+		if(fd[i][MS][WR] > 0 ) 
+			FD_SET(fd[i][SM][RD], set);		
+	} 
+}
+
+void closeOther(int me , int fd[QSLAVES][2][]){
 	for(int i = 0 ; i < me ; i++){
 		close(fd[i][SM][RD]);
 		close(fd[i][SM][WR]);
 		close(fd[i][MS][RD]);
 		close(fd[i][MS][WR]);
-		}	
-	
-
-
+	}	
 }
 
-int  convert ( char * src , char * dest) {
+int convert( char * src , char * dest) {
 	int i = 0 ; 
 	while(src[i]){
 		dest[i]=src[i];
@@ -42,129 +147,4 @@ int  convert ( char * src , char * dest) {
 	dest[i++]='\n';
 	dest[i]=0;
 	return i ;
-
-
 }
-
-
-
-
-
-void initializeSet(fd_set * set, int  fd[QSLAVES][2][2]);
-
-
-int main(int argc, char *argv[]) {
-  
-       
-    // Slave, flow direction, read|write
-    int pipes[QSLAVES][2][2]; // pipes[slave_id][ MS | SM ][ RD | WR ]
-    
-   for (int i=0; i<QSLAVES; i++) 
-        for (int j=0; j<2; j++)
-            if ( pipe(pipes[i][j]) < 0 ) {
-                perror("Unable to create pipe at master\n"); 
-                return 1; 
-            }  
-   
-    
-    pid_t spids[QSLAVES];     
-    char * args[] = {"./slave",NULL}; 
-    
-
-    for (int i=0; i<QSLAVES; i++) {
-        spids[i] = fork(); 
-	if ( spids[i] == -1 ) { 
-            perror("Unable to fork\n"); 
-            return 1; 
-        } else if ( !spids[i] ) {
-	    closeOther(i,pipes);
-            close(pipes[i][MS][WR]); 
-            close(pipes[i][SM][RD]); 
-            dup2(pipes[i][MS][RD], STDIN_FILENO);
-            dup2(pipes[i][SM][WR], STDOUT_FILENO);
-            execvp("./slave", args); 
-        } 
-        close(pipes[i][MS][RD]); 
-        close(pipes[i][SM][WR]); 
-    }
-
-	sleep(15);
-
-
-    	int active = QSLAVES;
-	int currentPathIndex = 1;
-	char pass[50];
-	for( int i = 0 ; i < QSLAVES ; i++){
-		if(currentPathIndex < argc ){
-			int count = convert(argv[currentPathIndex],pass);
-
-			if( write ( pipes[i][MS][WR] , pass , count ) != count)
-			   printf("Write goes wrong\n");
-			currentPathIndex++;
-		}else{
-			close(pipes[i][MS][WR]);
-			pipes[i][MS][WR]=-1;
-			active--;
-		}
-	
-	}	
-
-	
-      fd_set readings;
-      initializeSet(&readings,pipes);
-
-	int resfd = open("resultado",O_RDWR | O_CREAT,S_IRWXU);
-
-
-    	
-    int updated;
-    while(active){
-	updated = select( FD_SETSIZE ,&readings ,NULL ,NULL,NULL); 
-       	for(int i = 0 ; i < QSLAVES && updated ; i++){
-		if( FD_ISSET(pipes[i][SM][RD],&readings) )
-		{
-			updated--;		
-			if( currentPathIndex < argc){
-				int count = convert(argv[currentPathIndex],pass);
-				write(pipes[i][MS][WR],pass,count);					
-				currentPathIndex++;
-			}else{ 
-				close(pipes[i][MS][WR]);
-				pipes[i][MS][WR]=-1;
-				active--;
-			}
-		}
-	 }
-
-	for(int i = 0 ; i < QSLAVES ; i++){
-		if( FD_ISSET(pipes[i][SM][RD],&readings) ){
-		    	char tr[300];
-			int readC = read(pipes[i][SM][RD],tr,300);
-			dprintf(resfd,"Proccess number:\t%d\n",spids[i]);
-			write(resfd,tr,readC);
-		}	
-	
-	
-	} 
-
-    	initializeSet(&readings,pipes);
-    }
-
-
-    for ( int i = 0 ; i < QSLAVES ; i++){
-    	waitpid(spids[i],NULL,0);
-    }
-
-
-}
-
-	void initializeSet(fd_set *  set, int  fd[QSLAVES][2][2]){
-		FD_ZERO(set);
-		for(int i = 0 ; i < QSLAVES ; i++) {
-			if(fd[i][MS][WR] > 0 ) 
-				FD_SET(fd[i][SM][RD], set);		
-		} 
-	
-	}
-
-
--- slave.c
+++ slave.c
@@ -9,7 +9,7 @@
 
 // Compile with -DSOLVER = '"solver_path"'
 #ifndef SOLVER
-    #define SOLVER "/usr/bin/minisat"
+	#define SOLVER "/usr/bin/minisat"
 #endif
 
 // Compile with -DPARSER ='"cmd_parser"'
@@ -22,47 +22,43 @@
 // TODO: error library - index errors 
 int main(int argc, char *argv[]) {
 
-    char buffer[BUFFSIZE]; 
-    char * r; 
+	char buffer[BUFFSIZE]; 
+	char * r; 
 
-    do {
-        // El problema con usar getline es que arma un string en el heap, que despues hay que liberar. No esta bueno, es ineficiente. 
-        // Por otro lado, getline devuelve el # del buffer final; fgets no devuelve la # de caracteres repetidos
-        // Por otro lado, fgets puede trabajar con un buffer en el stack
-        r = fgets(buffer, BUFFSIZE, stdin);
-        int n = strlen(buffer); //INEFICIENTE! Hay que encontrar una manera de getline sin \n pero en el stack
-        buffer[n-1] = '\0';  
+	do {
+		r = fgets(buffer, BUFFSIZE, stdin);
+		int n = strlen(buffer); //INEFICIENTE! 
+		buffer[n-1] = '\0';  
 	
-	if( r == NULL ) //error or end of file.
-		return 0; 
+		if( r == NULL ) //error or end of file.
+			return 0; 
 
+		FILE * parser = popen(PARSER,"w");
+		if( parser == NULL) {
+			perror("Unable to popen the parser.\n");
+			return -1;
+		}
+		
+		int fd = fileno(parser);
+		if(fd < 0 ) {
+			perror("Unable to fileno\n");
+			return -1;
+		}
 
-        FILE * parser = popen(PARSER,"w");
-        if( parser == NULL) {
-		    perror("Unable to popen the parser.\n");
-		    return -1;
-	    }
-        
-        int fd = fileno(parser);
-        if(fd < 0 ) {
-		    perror("Unable to fileno\n");
-		    return -1;
-	    }
-
-	    pid_t pid = fork();
-        if ( pid < 0 ){
-		    perror("Unable to fork\n");
-		    return -1;
-	    } else if(pid==0){
-            dup2(fd, STDOUT_FILENO); //check if fails 
-            execl(SOLVER, SOLVER, buffer, NULL);
-            perror("Unable to exec solver\n");
-            return -1; 
-	    } else{
-	        int status; 
-            waitpid(pid,&status,0);
-	        pclose(parser);
-        }
-    } while( r != NULL );
-    return 0; 
+		pid_t pid = fork();
+		if ( pid < 0 ){
+			perror("Unable to fork\n");
+			return -1;
+		} else if(pid==0){
+			dup2(fd, STDOUT_FILENO); //check if fails 
+			execl(SOLVER, SOLVER, buffer, NULL);
+			perror("Unable to exec solver\n");
+			return -1; 
+		} else{
+			int status; 
+			waitpid(pid,&status,0);
+			pclose(parser);
+		}
+	} while( r != NULL );
+	return 0; 
 } 
